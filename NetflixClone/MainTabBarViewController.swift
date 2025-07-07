da0a02e01ebfe0c78a52829eb8841cdb1b97f88c
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-07-07 16:43:24 +0530

    add tabBarIcons and title

--- NetflixClone/MainTabBarViewController.swift
+++ NetflixClone/MainTabBarViewController.swift
@@ -11,8 +11,33 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         view.backgroundColor = .systemYellow
+        
+//        importing all the bottom tab screens
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        
+//        set image to tabs
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+//        set tabs title
+        vc1.title="Home"
+        vc2.title="Comming Soon"
+        vc3.title="Search"
+        vc4.title="Downloads"
+        
+        
+        
+//        display all the tab screens in array
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
+        
+        
     }
 
 
