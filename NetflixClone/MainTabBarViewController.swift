6540fe1510b019e7b666d5e750d90440f5ba8a1e
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-07-07 15:03:34 +0530

    initialise and remove main storyboard from project

---
+++ NetflixClone/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetflixClone
+//
+//  Created by V1 Macbook Air on 07/07/2025.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
---
+++ NetflixCloneTests/NetflixCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneTests.swift
+//  NetflixCloneTests
+//
+//  Created by V1 Macbook Air on 07/07/2025.
+//
+
+import Testing
+@testable import NetflixClone
+
+struct NetflixCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
